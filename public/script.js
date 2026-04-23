bbb8750be71f5ccf35bce8469b31cefe1b78c6ed
kietelmuis <omar.hopman9@gmail.com>
2026-04-23 20:24:32 +0200

    feat: render markdown

--- public/script.js
+++ public/script.js
@@ -1,25 +1,35 @@
+import { Parser, HtmlRenderer } from "https://esm.sh/commonmark";
+
+const parser = new Parser();
+const renderer = new HtmlRenderer();
+
 window.check = async function check() {
-  console.log("fgdgf");
-  const input = document.getElementById("input");
+    const input = document.getElementById("input");
 
-  const url = "http://localhost:3000/groq";
-  const response = await fetch(url, {
-    method: "POST",
-    body: input.value,
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-  });
-  const reader = response.body.getReader();
-  const decoder = new TextDecoder();
+    const url = "http://localhost:3000/groq";
+    const response = await fetch(url, {
+        method: "POST",
+        body: input.value,
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    });
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
 
-  let thing = document.getElementById("lechonk");
-  let text = document.createElement("p");
-  thing.append(text);
+    let messages = document.getElementById("lechonk");
+    let message = document.createElement("p");
+    messages.append(message);
 
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) break;
-    text.innerHTML += decoder.decode(value);
-  }
+    let fullText = "";
+
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+
+        fullText += decoder.decode(value);
+
+        const parsed = parser.parse(fullText);
+        message.innerHTML = renderer.render(parsed);
+    }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-import { Context, Elysia, file } from 'elysia'
-import { streamText } from 'ai'
-import { createGroq } from '@ai-sdk/groq';
-import { staticPlugin} from '@elysiajs/static'
+import { Context, Elysia, file } from "elysia";
+import { streamText } from "ai";
+import { createGroq } from "@ai-sdk/groq";
+import { staticPlugin } from "@elysiajs/static";
 
 const groq = createGroq({
-  apiKey: process.env.lekey
+    apiKey: process.env.lekey,
 });
 
 new Elysia()
-    .post('/groq',  async ({ request }: Context) => {
-    const stream = streamText({
-      model: groq("meta-llama/llama-4-scout-17b-16e-instruct"),
-      system: "You are Hina Sorasaki",
-      prompt: await request.text(),
-    });
+    .post("/groq", async ({ request }: Context) => {
+        const stream = streamText({
+            model: groq("meta-llama/llama-4-scout-17b-16e-instruct"),
+            system: "You are Hina Sorasaki",
+            prompt: await request.text(),
+        });
 
-    return stream.textStream;
-})
+        return stream.textStream;
+    })
 
-.use(staticPlugin({ assets: "public", prefix: "" }))
-.use(staticPlugin())
-.listen(3000);
+    .use(staticPlugin({ assets: "public", prefix: "" }))
+    .listen(3000);
