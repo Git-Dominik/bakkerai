5a7c0b9473ae5f7f65da5f405c3645ce70322b1c
kietelmuis <omar.hopman9@gmail.com>
2026-06-03 15:25:52 +0200

    svh api

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,27 @@ import jwt from "@elysiajs/jwt";
 import { authRoutes } from "./auth";
 import Elysia, { file } from "elysia";
 import { chatRoutes } from "./chats";
+import { compRoutes } from "./comps";
+
+import { login, getRecipesCategories, getRecipes, getRecipe } from "./api/svh";
+
+const cookie = await login("c.lust@talland.nl", "Welkom123!");
+console.log(cookie.value);
+
+const categories = await getRecipesCategories(cookie, "Brood");
+console.log(categories);
+
+const recipes = await getRecipes(cookie, "Brood", categories[0]);
+console.log(recipes);
+
+const recipe = await getRecipe(cookie, recipes[0]);
+console.log(recipe);
 
 const api = new Elysia({ prefix: "/api" })
   .use(jwt({ name: "jwt", secret: "SuperSecretKey" }))
   .use(authRoutes)
-  .use(chatRoutes);
+  .use(chatRoutes)
+  .use(compRoutes);
 
 new Elysia()
   .use(api)
